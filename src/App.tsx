036aa2766581132b32ff7df07907c220cd9d579e
SohamHUG <69.hugue@gmail.com>
2025-04-29 16:30:07 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <p className="bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-5xl font-extrabold text-transparent ...">
+        Hello world
+      </p>
+    </>
+  )
+}
+
+export default App
